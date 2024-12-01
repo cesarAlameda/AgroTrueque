6ee88cd3d335c10fdb1d95b095c2815912f9fac4
cesarAlameda <147344298+cesarAlameda@users.noreply.github.com>
2024-12-01 15:27:26 +0100

    Bug 1-Arreglado

--- app/src/main/java/com/csaralameda/agrotrueque/MainActivity.java
+++ app/src/main/java/com/csaralameda/agrotrueque/MainActivity.java
@@ -47,14 +47,24 @@ public class MainActivity extends AppCompatActivity {
 
             BottomNavigationView navView = findViewById(R.id.nav_view);
             AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                    R.id.navigation_perfil, R.id.navigation_anuncios, R.id.navigation_notifications)
                     .build();
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             NavigationUI.setupWithNavController(binding.navView, navController);
-
             perfilViewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
             perfilViewModel.setUser(user);
+
+            //RECARGO LA VISTA PARA QUE ESTÉ CON LOS DATOS GARGADOS
+            navController.navigate(R.id.navigation_anuncios);
+            navController.navigate(R.id.navigation_perfil);
+
+
+
+
+
+
+
 
 
         }
